cee271cdd0b28f30ef60b76b27ddcb3f56d4d8ac
Mandeep Singhmar <msingh8328@gmail.com>
2024-03-03 16:16:39 +0530

    init

--- src/content.tsx
+++ src/content.tsx
@@ -1,11 +1,53 @@
-import cssText from "data-text:~style.css"
-import type { PlasmoCSConfig } from "plasmo"
+// import cssText from "data-text:~style.css"
+// import type {
+//   PlasmoCSConfig,
+//   PlasmoCSUIJSXContainer,
+//   PlasmoCSUIProps,
+//   PlasmoRender
+// } from "plasmo"
+// import type { FC } from "react"
+// import { createRoot } from "react-dom/client"
 
-import { CountButton } from "~features/count-button"
+// import { LinkedinAi } from "~features/linkedin-ai"
+
+// export const config: PlasmoCSConfig = {
+//   matches: ["https://*.linkedin.com/*"]
+// }
+// export const getStyle = () => {
+//   const style = document.createElement("style")
+//   style.textContent = cssText
+//   return style
+// }
+
+// export const getRootContainer = () =>
+//   document.querySelector(".msg-form__msg-content-container")
+
+// const PlasmoOverlay: FC<PlasmoCSUIProps> = () => {
+//   return (
+//       <LinkedinAi />
+//   )
+// }
+
+// export const render: PlasmoRender<PlasmoCSUIJSXContainer> = async ({
+//   createRootContainer
+// }) => {
+//   const rootContainer = await createRootContainer()
+//   const root = createRoot(rootContainer)
+//   root.render(<PlasmoOverlay />)
+// }
+
+// export default PlasmoOverlay
+import cssText from "data-text:~style.css"
+import type { PlasmoCSConfig, PlasmoGetOverlayAnchor } from "plasmo"
+
+import { LinkedinAi } from "~features/linkedin-ai"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://*.linkedin.com/*"]
 }
+
+export const getOverlayAnchor: PlasmoGetOverlayAnchor = async () =>
+  document.querySelector(".msg-form__msg-content-container")
 
 export const getStyle = () => {
   const style = document.createElement("style")
@@ -15,8 +57,8 @@ export const getStyle = () => {
 
 const PlasmoOverlay = () => {
   return (
-    <div className="z-50 flex fixed top-32 right-8">
-      <CountButton />
+    <div>
+      <LinkedinAi />
     </div>
   )
 }
